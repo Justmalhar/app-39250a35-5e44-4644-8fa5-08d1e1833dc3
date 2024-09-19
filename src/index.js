75aa1fe4e3785b5af44ab68dcb34b8a93b064047
Malhar Ujawane <Malharujawane@gmail.com>
2024-09-18 23:52:00 -0700

    Initial commit for ai-assistant

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+/**
+ * Main Entry Point for the App
+ * Renders the App component to the root element
+ */
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+        
